1d31984d41280680500979434fdcbca61bd9cf22
André Nascimento <andre4.20nascimento@outlook.com>
2023-03-04 13:47:17 -0300

    Contagem Regressiva

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+Console.WriteLine("Apontar !"); Thread.Sleep(3000);
+Console.WriteLine();
+Console.WriteLine("Preparar !"); Thread.Sleep(3000);
+Console.WriteLine();
+Console.WriteLine("Fogo !");
